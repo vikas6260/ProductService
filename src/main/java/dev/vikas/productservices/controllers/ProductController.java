2613e3fb9721568e80d5c37dde332fcc9f658576
vikas <vikas.singh@xecurify.com>
2025-05-16 22:22:58 +0530

    product services

--- src/main/java/dev/vikas/productservices/controllers/ProductController.java
+++ src/main/java/dev/vikas/productservices/controllers/ProductController.java
@@ -1,2 +1,11 @@
-package dev.vikas.productservices.controllers;public class ProductController {
+package dev.vikas.productservices.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+
 }
--- src/main/java/dev/vikas/productservices/controllers/SampleController.java
+++ src/main/java/dev/vikas/productservices/controllers/SampleController.java
@@ -1,2 +1,19 @@
-package dev.vikas.productservices.controllers;public class SampleController {
+package dev.vikas.productservices.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+    @GetMapping("/1")
+
+    public String sample() {
+        return "hello worl !";
+    }
+    @GetMapping("/2")
+    public String sample2() {
+        return "hello world 2 !";
+    }
 }
