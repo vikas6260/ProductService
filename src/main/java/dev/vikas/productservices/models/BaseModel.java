0688cc73ed61238ce1c6b46c527b9300821064da
vikas <vikas.singh@xecurify.com>
2025-05-21 11:39:35 +0530

    base-model-getter-setter

--- src/main/java/dev/vikas/productservices/models/BaseModel.java
+++ src/main/java/dev/vikas/productservices/models/BaseModel.java
@@ -1,7 +1,12 @@
 package dev.vikas.productservices.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class BaseModel {
     private long id;
     private Date createdAt;
